6ee1716bfee20d4b628de10d9a226f18d4188b03
tshaynik <tshaynik@protonmail.com>
2021-10-06 10:47:01 -0400

    add devShell with node version 12

--- flake.nix
+++ flake.nix
@@ -11,8 +11,10 @@
     };
   };
 
-  outputs = { self, nixpkgs, npmlock2nix-repo }: {
-
+  outputs = { self, nixpkgs, npmlock2nix-repo }:
+    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+  {
     overlay = final: prev:
       let
         npmlock2nix = import npmlock2nix-repo { pkgs = prev; };
@@ -28,6 +30,11 @@
       };
     };
 
+    devShell.x86_64-linux = pkgs.mkShell {
+      buildInputs = [
+        pkgs.nodejs-12_x
+      ];
+    };
 
     packages.x86_64-linux.my-jitsi-meet =
     let
@@ -36,6 +43,5 @@
       pkgs.my-jitsi-meet;
 
     defaultPackage.x86_64-linux = self.packages.x86_64-linux.my-jitsi-meet;
-
   };
 }
